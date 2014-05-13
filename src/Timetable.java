321b90933115e06558c25ff76e8ac4cf1fdec11c
dedales <mr.ladizhin@mail.ru>
2014-05-13 11:53:33 -0700

    Update Timetable.java

--- src/Timetable.java
+++ src/Timetable.java
@@ -6,7 +6,7 @@ public class Timetable
 	String s;
 	//static Lesson[] ls = new Lesson[20];
 	static Lesson ls;
-	static String[] week = {"Понеділок","Вівторок","Середа","Четверг","П'ятниця"};
+	static String[] week = {"Понеділок","Вівторок","Середа","Четверг","П'ятниця","Субота"};
 	
 	public Timetable(String s)
 	{
@@ -29,10 +29,10 @@ public class Timetable
 		while (sc.hasNext()) 
 		{
 			System.out.println(week[n]);
-			for(int i = 0; i < 4; i++)
+			for(int i = 0; i < 5; i++)
 			{
 				t = (sc.nextLine().split("\\|"));
-				if (t.length != 4) continue;
+				if (t.length != 5) continue;
 				ls = new Lesson(Integer.parseInt(t[1]), Integer.parseInt(t[2]));
 				System.out.println(t[0] + ". " + ls.getNS() + " / " +
 				ls.getNL() + " / " + t[3]);
